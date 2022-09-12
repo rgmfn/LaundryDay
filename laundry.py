c0c878f020237f4ea1fa07c30ea8704c2b4a0e8f
ubuntu <rygragg@gmail.com>
2022-09-11 23:41:22 -0700

    pair dissappear

--- laundry.py
+++ laundry.py
@@ -1,7 +1,7 @@
 import pygame
 
 # TODO
-#  -make wood floor
+#  -make (better) wood floor
 
 pygame.init()
 
@@ -21,27 +21,37 @@ pygame.display.set_caption('Laundry Day!')
 
 class Sock:
 
-    def __init__(self, num, color, rect):
+    def __init__(self, num, rect):
         self.pair = None
         self.img = pygame.image.load(f'art/sock{num}.png').convert_alpha()
-        self.color = color
         self.x = rect[0]
         self.y = rect[1]
-        self.w = rect[2] * 3
-        self.h = rect[3] * 3
+        self.w = rect[2] * 10
+        self.h = rect[3] * 10
         self.img = pygame.transform.scale(self.img, (self.w, self.h))
 
     def get_rect(self):
-        return (self.x, self.y, self.w, self.h)
+        return pygame.Rect(self.x, self.y, self.w, self.h)
 
-sock1 = Sock(1, (255, 0, 0), (30, 30, 20, 20))
-sock3 = Sock(2, (0, 255, 0), (60, 60, 20, 20))
+sock1 = Sock(3, (30, 30, 5, 8))
+sock2 = Sock(3, (30, 120, 5, 8))
+sock1.pair = sock2
+sock2.pair = sock1
+sock3 = Sock(4, (60, 60, 5, 8))
+sock4 = Sock(4, (60, 120, 5, 8))
+sock3.pair = sock4
+sock4.pair = sock3
 
-socks = [sock1, sock3]
+socks = [sock1, sock2, sock3, sock4]
 held_sock = None
+
+floor_img = pygame.image.load('art/floor1.png').convert_alpha()
+floor_img = pygame.transform.scale(floor_img, (600, 600))
 
 """
 gets top item from pile that is under cursor
+
+returns item
 """
 def get_top_item(pile):
     mx, my = pygame.mouse.get_pos()
@@ -52,6 +62,20 @@ def get_top_item(pile):
                 return item
 
     return None
+
+"""
+sees if two items/socks overlap
+
+returns bool
+"""
+def overlaps(a, b):
+    if not (a and b):
+        return False
+
+    if (not a) or (not b):
+        return False
+
+    return a.get_rect().colliderect(b.get_rect())
 
 mainClock = pygame.time.Clock()
 
@@ -92,9 +116,28 @@ while run:
 
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == MOUSE_LEFT:
+                # check if match:
+                # 1. make sure held sock and its pair overlap
+                if overlaps(held_sock, held_sock.pair):
+                    # 2. find other pair in socks arr
+                    held_pair_index = socks.index(held_sock.pair)
+                    held_pair = socks[held_pair_index]
+                    # 3. make sure no socks between them overlap the pair sock
+                    #      (the pair sock is on top)
+                    no_between = True
+                    for i in range(held_pair_index+1, len(socks)-1):
+                        if overlaps(held_pair, socks[i]):
+                            no_between = False
+                            break
+
+                    if no_between:
+                        socks.remove(held_pair)
+                        socks.pop()
+
                 held_sock = None
 
     display.fill(C_BROWN)  # white
+    display.blit(floor_img, (0, 0))
 
     # delta = pygame.mouse.get_rel()
     # print(f'dx: {delta[0]}, dy: {delta[1]}')
